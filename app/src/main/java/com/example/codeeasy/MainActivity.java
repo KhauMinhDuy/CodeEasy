963c3eb02b211a327d0612d4fe461aaca08231a4
khauminhduy <khauminhduy@gmail.com>
2020-05-23 20:00:08 +0700

    initialize commit

---
+++ app/src/main/java/com/example/codeeasy/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.codeeasy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int REQUEST_CODE = 1;
+
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private EditText _edEmail, _edPassword;
+    private Button _btnLogin, _btnRegister, _btnLoginGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        setControl();
+        setupFirebaseAuth();
+        setEvent();
+    }
+
+    /*
+    -----------------Control--------------------------
+     */
+
+    private void setControl() {
+        _edEmail = findViewById(R.id.editText_Email);
+        _edPassword = findViewById(R.id.editText_Password);
+        _btnLogin = findViewById(R.id.button_Login);
+        _btnRegister = findViewById(R.id.button_Register);
+        _btnLoginGoogle = findViewById(R.id.button_SignIn_gg);
+    }
+
+    /*
+         ---------------------EVENT-----------------------
+      */
+    private void setEvent() {
+        _btnLogin.setOnClickListener(listener);
+        _btnRegister.setOnClickListener(listener);
+        _btnLoginGoogle.setOnClickListener(listener);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.button_Login:
+//                    Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                    signIn(_edEmail.getText().toString(), _edPassword.getText().toString());
+                    break;
+                case R.id.button_Register:
+                    Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                    startActivityForResult(intent, REQUEST_CODE);
+                    break;
+                case R.id.button_SignIn_gg:
+                    Toast.makeText(MainActivity.this, "Sign In Google", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    /*
+    -------------------METHOD-------------------------
+     */
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            String email = data.getStringExtra("email");
+            String password = data.getStringExtra("password");
+            _edEmail.setText(email);
+            _edPassword.setText(password);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthStateListener != null) {
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener);
+    }
+
+    /*
+    -------------------FireBaseAuth----------------------
+    */
+    private void setupFirebaseAuth() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    if (user.isEmailVerified()) {
+                        Toast.makeText(MainActivity.this, "Email success", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "Check email", Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                    }
+                } else {
+                    Log.d("Login", "Signin_out");
+                }
+            }
+        };
+    }
+
+    private void signIn(String email, String password) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(MainActivity.this, "Sign In Success", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Sign In Fail", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/codeeasy/RegisterActivity.java
@@ -0,0 +1,138 @@
+package com.example.codeeasy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static int REQUEST_CODE = 1;
+    private static String TAG = "Register";
+    private static String DOMAIN_NAME = "gmail.com";
+    private FirebaseAuth mAuth;
+    private EditText _mEmail, _mPassword, _mConfigPassword;
+    private Button _mRegister;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        setControl();
+        setEvent();
+    }
+
+    private void setControl() {
+        _mEmail = findViewById(R.id.editText_register_email);
+        _mPassword = findViewById(R.id.editText_register_password);
+        _mConfigPassword = findViewById(R.id.editText_register_cfpassword);
+        _mRegister = findViewById(R.id.button_register_real);
+    }
+
+    private void setEvent() {
+        _mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check null edittext fields
+                if (!isEmpty(_mEmail.getText().toString()) && !isEmpty(_mPassword.getText().toString()) && !isEmpty(_mConfigPassword.getText().toString())) {
+                    if (isEmail(_mEmail.getText().toString())) {
+                        if (isMatchPassword(_mPassword.getText().toString(), _mConfigPassword.getText().toString())) {
+                            Log.d("Register", "Click");
+                            createNewEmail(_mEmail.getText().toString().trim(), _mPassword.getText().toString().trim());
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Password do not match", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Please Register with Gmail", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(RegisterActivity.this, "You nust fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private boolean isEmpty(String string) {
+        return string.equals("");
+    }
+
+    private boolean isEmail(String email) {
+        String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+        return DOMAIN_NAME.equals(domain);
+    }
+
+    private boolean isMatchPassword(String pw1, String pw2) {
+        return pw1.equals(pw2);
+    }
+
+    private void createNewEmail(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Register", "" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            sendEmail();
+                            mAuth.signOut();
+                            redirectLogin();
+                        } else {
+                            Log.d("Register", "Fail");
+                        }
+                    }
+                }
+        );
+
+    }
+
+    private void redirectLogin() {
+        String email = _mEmail.getText().toString();
+        String password = _mPassword.getText().toString();
+        Intent i = new Intent();
+        i.putExtra("email", email);
+        i.putExtra("password", password);
+        setResult(REQUEST_CODE, i);
+        finish();
+    }
+
+    /*
+       ------------------------Send Email------------------------
+        */
+    private void sendEmail() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification()
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(RegisterActivity.this, "Send Email success", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "Send Email Fail", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+
+}
